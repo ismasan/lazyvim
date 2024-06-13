a79721559c445336bffbb7c0f8604147e84b31ab
Ismael Celis <ismaelct@gmail.com>
2024-06-13 11:48:44 +0100

    don't need the manual setup

--- lua/plugins/slim_templates.lua
+++ lua/plugins/slim_templates.lua
@@ -1,9 +1,3 @@
--- set file type on buffer creation and reading
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = { "*.slim" },
-  command = "set ft=slim",
-})
-
 -- lazy spec
 return {
   "slim-template/vim-slim", -- Syntax highlighting for VIM
